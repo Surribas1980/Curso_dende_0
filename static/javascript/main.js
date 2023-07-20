1096a12f8bd2e62090799dbf49bc1eeceaafa0ba
Surribas1980 <issupl@gmail.com>
2023-07-20 10:59:42 +0000

    Clase mercores 19 xullo 2023 As Pontes

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,14 +1,27 @@
 function escribir(entradaDeTexto){
   
   
-  let div = document.createElement('div');
+  let unP = document.createElement('li');// o non existir un li, agrego unha referencia
+  let check = document.createElement('input');
+  check.setAttribute('type','checkbox')
+  check.classList.add("novo-accent-color");
+  //unP.classList.add("cambioCor")
+  unP.classList.add("en-linea")
+  console.log('a entrada de texto en escribir é: ☢',entradaDeTexto)
+  console.log('estou a crear un unP: ',unP);
 
-  console.log('a entrada de texto en escribir é: ',entradaDeTexto)
-  console.log('estou a crear un div: ',div);
+  unP.innerHTML = entradaDeTexto;
 
-  div.innerHTML = entradaDeTexto;
 
-  document.body.append(div)
+  
+  unP.addEventListener('click',changeColor)
+
+  
+  /*document.body.append(div)
+  document.body.append(check)*/
+  /*parrafos.prepend(unP)*/
+  parrafos.append(unP)
+  //parrafos.before(check)
 }
 
 const escribindoDendeOBoton = () => {
@@ -16,5 +29,9 @@ const escribindoDendeOBoton = () => {
   console.log(entradaDeDatos.value)
   
   escribir(entradaDeDatos.value)
+}
+
+const changeColor = (event) => {
+  event.target.classList.toggle("cambioCor")
 }
 
